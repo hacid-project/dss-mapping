ea6770bbd483d689b9816dcf6c051c8d8ed66384
Miguel Ceriani <miguel.ceriani@gmail.com>
2026-02-11 23:52:45 +0100

    Include context from Python call

--- src/dss_mapping/map.py
+++ src/dss_mapping/map.py
@@ -3,11 +3,15 @@ from pydantic import BaseModel
 import pathlib
 
 def model_to_jsonld(
-    model: BaseModel
+    model: BaseModel,
+    include_context: bool = True
 ):
     print(pathlib.Path(__file__).parent.resolve())
     return jq.compile(
-        'include "map"; .type = $type | map',
+        'include "map"; ' +
+        ('include "context"; ' if include_context else '') +
+        '.type = $type | map' +
+        (' | ."@context" = context' if include_context else ''),
         args={"type": type(model).__name__},
         library_search_path=[f'{pathlib.Path(__file__).parent.resolve()}/jq']
     ).input_value(model.model_dump()).first()
