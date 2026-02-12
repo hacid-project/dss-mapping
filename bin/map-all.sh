afdfe1583146bcbdcba2861e5d35dc13838a970d
Miguel Ceriani <miguel.ceriani@gmail.com>
2026-02-12 11:22:23 +0100

    Fix jq queries directory

--- bin/map-all.sh
+++ bin/map-all.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-JQ_DIR="lib"
+JQ_DIR="src/dss_mapping/jq"
 INPUT_DIR="data/download"
 # INPUT_DIR="data/source-sample"
 OUTPUT_DIR="data/rdf"
 #MAPPING_DIR="config/mappings"
 #JSONLD_OUTPUT_DIR="$OUTPUT_DIR/jsonld"
-JSONLD_OUTPUT="$OUTPUT_DIR/all.jsond"
+JSONLD_OUTPUT="$OUTPUT_DIR/all.jsonld"
 # NQ_OUTPUT_DIR="$OUTPUT_DIR/nt"
 # NQ_GLOBAL_OUTPUT="$OUTPUT_DIR/all.nt"
 # LOG_PERIOD=50
@@ -18,5 +18,5 @@ JSONLD_OUTPUT="$OUTPUT_DIR/all.jsond"
 mkdir -p "$OUTPUT_DIR"
 for INPUT in $INPUT_DIR/*.json; do
     jq -L "$JQ_DIR" 'include "map"; .[] | map' --compact-output  <$INPUT
-done | jq -L "$JQ_DIR" --slurp 'include "context"; {"@context": context, "@graph": .}' >$OUTPUT_DIR/all.jsonld
+done | jq -L "$JQ_DIR" --slurp 'include "context"; {"@context": context, "@graph": .}' >$JSONLD_OUTPUT
 
